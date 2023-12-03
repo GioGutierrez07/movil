702d02c2f7ba29d97f4d7965837ce6bc1b3dfbef
Brayan Díaz <s21120009@alumnos.itsur.edu.mx>
2023-12-03 00:16:52 -0600

    Se agrego un componente para las notificaciones, funciona como una alerta.

--- app/src/main/java/com/example/movil/viewModels/AlarmReceiverNotificacion.kt
+++ app/src/main/java/com/example/movil/viewModels/AlarmReceiverNotificacion.kt
@@ -1,4 +1,16 @@
 package com.example.movil.viewModels
 
-class AlarmReceiverNotificacion {
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiverNotificacion : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val notification = intent?.getParcelableExtra<Notification>("EXTRA_NOTIFICATION")
+        val notificationManager =
+            context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
+    }
 }
