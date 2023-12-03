d4b30a848f817f62a1692f6e2f3aab453befdfe2
Brayan Díaz <s21120009@alumnos.itsur.edu.mx>
2023-12-02 23:52:24 -0600

    Complementacion del codigo de NotificacionWiewModel.

--- app/src/main/java/com/example/movil/viewModels/NotificacionWiewModel.kt
+++ app/src/main/java/com/example/movil/viewModels/NotificacionWiewModel.kt
@@ -6,6 +6,8 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -45,5 +47,20 @@ class NotificacionWiewModel : ViewModel() {
             intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                fechaEjecucion,
+                pendingIntent
+            )
+        } else {
+            alarmManager.setExact(
+                AlarmManager.RTC_WAKEUP,
+                fechaEjecucion,
+                pendingIntent
+            )
+        }
+
+        Log.e("Alarm", "Alarm scheduled for $fechaEjecucion")
     }
 }
